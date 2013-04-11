827d0c9747f1c179f4b21b6f771ef62a95bc4f53
pjcooke_cp <SND\pjcooke_cp>
2013-04-11 22:15:02 +0000

    Tweetsharp in progress - authentication complete

--- Twitter/Pages/Settings.xaml.cs
+++ Twitter/Pages/Settings.xaml.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Windows;
 using System.Windows.Controls;
 using Tweety.Core;
+using TweetSharp;
 
 namespace Pages
 {
@@ -11,43 +12,24 @@ namespace Pages
     /// </summary>
     public partial class Settings : Window
     {
-        private string OAuthToken { get; set; }
+        TwitterService service;
+        OAuthRequestToken requestToken ;
 
         public Settings() {
             InitializeComponent();
 
-            OAuthTwitter oAuth = new OAuthTwitter();
-            oAuth.ConsumerKey = SettingHelper.ConsumerKey;
-            oAuth.ConsumerSecret = SettingHelper.ConsumerSecret;
-
-            //Get the authorization url.
-            Uri url = new Uri(oAuth.AuthorizationLinkGet());
-
-            //Save the token.
-            OAuthToken = HttpUtility.ParseQueryString(url.Query)["oauth_token"];
-
-            //Show the intro text with the authorization link.
-            string HTML = "<html><head><title></title><style type='text/css'>body {background-color:#5599BB;color:#ffffff;font-family:arial;}img{border:none}</style></head><body>"
-                + "<h1>Tweety Authorisation</h1><p>To use this application, you must first login to Twitter.</p>"
-                + "<p>Click the login button below and the Twitter login page will appear.</p>"
-                + "<p>After you login, you will be provided with a PIN.</p>"
-                + "<p>Please enter the PIN in the box below and click Ok.</p>"
-                + "<p><a href=\"" + url.ToString() + "\"><img src=\"http://apiwiki.twitter.com/f/1242697608/Sign-in-with-Twitter-lighter.png\" alt=\"Sign in with Twitter\" /></a></p>"
-                + "</body></html>";
-
-            WebBrowser.NavigateToString(HTML);
+            service = new TwitterService(SettingHelper.ConsumerKey, SettingHelper.ConsumerSecret);
+            requestToken = service.GetRequestToken();
+            Uri uri = service.GetAuthorizationUri(requestToken);
+            WebBrowser.Navigate(uri);
         }
 
         private void btnOk_Click(object sender, RoutedEventArgs e) {
             try {
-                OAuthTwitter oAuth = new OAuthTwitter();
-                oAuth.ConsumerKey = SettingHelper.ConsumerKey;
-                oAuth.ConsumerSecret = SettingHelper.ConsumerSecret;
-                oAuth.Token = OAuthToken;
-                oAuth.AccessTokenGet(OAuthToken, txtUpdate.Text);
+                OAuthAccessToken access = service.GetAccessToken(requestToken, txtUpdate.Text);
 
-                SettingHelper.Token = oAuth.Token;
-                SettingHelper.TokenSecret = oAuth.TokenSecret;
+                SettingHelper.Token = access.Token;
+                SettingHelper.TokenSecret = access.TokenSecret;
                 SettingHelper.Save();
                 this.Close();
             }
