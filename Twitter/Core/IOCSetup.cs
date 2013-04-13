59e3e42523f201aaf0cb524bf9e7159e6fb65e8a
pjcooke_cp <SND\pjcooke_cp>
2013-04-13 15:21:57 +0000

    Old Twitter api references/files removed.

--- Twitter/Core/IOCSetup.cs
+++ Twitter/Core/IOCSetup.cs
@@ -4,15 +4,6 @@
     {
         public static void Setup() {
             // Setup the Ioc container
-            Ioc.Register<OAuthTwitter>(() => {
-                OAuthTwitter oAuthTwitter = new OAuthTwitter();
-                oAuthTwitter.ConsumerKey = SettingHelper.ConsumerKey;
-                oAuthTwitter.ConsumerSecret = SettingHelper.ConsumerSecret;
-                oAuthTwitter.Token = SettingHelper.Token;
-                oAuthTwitter.TokenSecret = SettingHelper.TokenSecret;
-                return oAuthTwitter;
-            });
-
             Ioc.Register<TweetSharp.TwitterService>(() => {
                 var service = new TweetSharp.TwitterService(SettingHelper.ConsumerKey, SettingHelper.ConsumerSecret);
                 service.AuthenticateWith(SettingHelper.Token, SettingHelper.TokenSecret);
